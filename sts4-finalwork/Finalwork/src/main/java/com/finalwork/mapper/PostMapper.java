d92d5f062d20acd5e161e0d81109fee6792b67bc
tlsqltnwls <tlsqltnwls@naver.com>
2022-11-11 23:48:40 +0900

    post table 생성 및 연결

---
+++ sts4-finalwork/Finalwork/src/main/java/com/finalwork/mapper/PostMapper.java
@@ -0,0 +1,19 @@
+package com.finalwork.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.finalwork.domain.PostDTO;
+
+// Post 테이블과 통신하기 위한 인터페이스
+@Mapper
+public interface PostMapper {
+
+	public int insertPost(PostDTO params);		// 게시글 생성
+	public PostDTO selectPostDetail(Long idx);	// 하나의 게시글 조회
+	public int updatePost(PostDTO params);		// 게시글 수정
+	public int deletePost(Long idx);			// 게시글 삭제
+	public List<PostDTO> selectPostList();		// 게시글 목록 조회
+	public int selectPostTotalCount();			// 삭제여부(delete_yn)가 'N'으로 지정된 게시글 개수 조회
+}
